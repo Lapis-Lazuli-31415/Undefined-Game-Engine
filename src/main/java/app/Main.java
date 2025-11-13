297ac38cb71ce5fb5f620c64a46a69f01d9f806c
massah <arafeh.massah2007@gmail.com>
2025-11-12 23:17:36 -0500

    setup fixed

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -6,11 +6,15 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
+        // The FXML is located in src/main/resources/Main/hello-view.fxml
+        // Use an absolute classpath path so the resource is found at runtime.
+        URL fxmlLocation = Main.class.getResource("/Main/hello-view.fxml");
+        FXMLLoader fxmlLoader = new FXMLLoader(fxmlLocation);
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
