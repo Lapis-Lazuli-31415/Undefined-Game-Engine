f560fed8651a9f1e52dce29417dd803f3711cd98
MSTNDL <140838491+MSTNDL@users.noreply.github.com>
2025-11-24 14:27:33 -0500

    ViewManager class added currently empty and basic Controller for create scene use case implemented

--- src/main/java/interface_adapter/create_scene/CreateSceneController.java
+++ src/main/java/interface_adapter/create_scene/CreateSceneController.java
@@ -1,4 +1,16 @@
 package interface_adapter.create_scene;
 
+import use_case.create_scene.CreateSceneInputBoundary;
+import use_case.create_scene.CreateSceneInputData;
+
 public class CreateSceneController {
+    private final CreateSceneInputBoundary createSceneInteractor;
+
+    public CreateSceneController(CreateSceneInputBoundary createSceneInteractor) {
+        this.createSceneInteractor = createSceneInteractor;
+    }
+
+    public void createScene(String name) {
+        createSceneInteractor.execute(new CreateSceneInputData(name));
+    }
 }
---
+++ src/main/java/view/ViewManager.java
@@ -0,0 +1,11 @@
+package view;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class ViewManager implements PropertyChangeListener {
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        // TODO method to complete
+    }
+}
